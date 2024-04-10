5f8c07fba2b4cdcfdd845e2d7c372a46270f98fa
Christian Q. Cleofas <cleofastiano27@gmail.com>
2024-04-10 21:04:23 +0800

    NO_TICKET - Fix design

--- Django/backend/app/urls.py
+++ Django/backend/app/urls.py
@@ -6,8 +6,9 @@ urlpatterns = [
     # Frontend API for Inventory
     url(r'^inventory$',views.inventoryApiRequest),
     url(r'^inventory/([0-9]+)$',views.inventoryApiRequest),
+    
     # Frontend API for Transaction
-    path('login_page', views.login_page, name='login_page'),
+    path('', views.login_page, name='login_page'),
     path('logout/', views.logout_view, name='logout'),
     path('dashboard', views.dashboard, name='dashboard'),
 ]
