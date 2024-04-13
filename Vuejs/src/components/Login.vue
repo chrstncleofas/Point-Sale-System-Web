0d995472fa7af7157eb3fd55f43511cfafbbcab6
Christian Q. Cleofas <cleofastiano27@gmail.com>
2024-04-13 17:38:50 +0800

    NO_TICKET - Fix path in vue login

--- Vuejs/src/components/Login.vue
+++ Vuejs/src/components/Login.vue
@@ -44,7 +44,7 @@ const toast = useToast();
 
 const login = async () => {
     try {
-        const response = await axios.post('http://127.0.0.1:8000/api/login', {
+        const response = await axios.post('http://127.0.0.1:8000/login', {
             username: username.value,
             password: password.value
         });
