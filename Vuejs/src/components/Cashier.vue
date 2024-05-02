2c8da8738eaff7ee9835aed07b3f18696a262af6
Christian Q. Cleofas <cleofastiano27@gmail.com>
2024-05-02 22:12:45 +0800

    NO_TICKET - Add not found page

--- Vuejs/src/components/Cashier.vue
+++ Vuejs/src/components/Cashier.vue
@@ -9,7 +9,10 @@
       </div>
    </div>
    <div v-else>
-      <p>Please login to access the cashier.</p>
+      <div class="flex flex-col items-center justify-center h-screen">
+         <h1 class="text-4xl font-bold text-red-500">404 Not Found</h1>
+         <p class="text-lg text-gray-600 mt-4">The requested page could not be found.</p>
+      </div>
    </div>
 </template>
 
--- Vuejs/src/components/Product.vue
+++ Vuejs/src/components/Product.vue
@@ -9,6 +9,12 @@
           <button type="submit" @click="addToCart(product)" class="bg-brightRed shadow-box w-20 h-7 rounded mt-4 float-right text-xs text-white btn-Add">Add to Cart</button>
       </div>
   </div>
+  <div v-else>
+      <div class="flex flex-col items-center justify-center h-screen">
+         <h1 class="text-4xl font-bold text-red-500">404 Not Found</h1>
+         <p class="text-lg text-gray-600 mt-4">The requested page could not be found.</p>
+      </div>
+   </div>
 </template>
 
 <script setup>
